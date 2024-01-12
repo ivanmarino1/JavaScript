0cadd5edd7bb3aa940a39a026acfaf4dc4e5dd75
ivanmarino1 <ivanamarillamarino@gmail.com>
2024-01-11 21:13:23 -0300

    commit

---
+++ JavaScript/02-Variables/js/03-app.js
@@ -0,0 +1,11 @@
+const producto = 'Tablet';
+
+// Las variables no se pueden reasignar.
+
+// producto = 'Monitor';
+
+console.log(producto);
+
+// Si o si se tienen que inicializar las variables.
+const precio = 20;
+console.log(precio);
