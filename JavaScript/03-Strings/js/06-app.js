bb7ac4c67700f506f1ec626f74757e7bf908f546
ivanmarino1 <ivanamarillamarino@gmail.com>
2024-01-12 23:44:44 -0300

    commit

---
+++ JavaScript/03-Strings/js/06-app.js
@@ -0,0 +1,19 @@
+const producto = 'Monitor 20 Pulgadas';
+
+// .repeat permite repetir una cadena de texto...
+
+const texto = ' en Promoción'.repeat();
+
+console.log(texto);
+console.log(`${producto} ${texto} !!!`);ç
+
+// Split permite dividir un string
+
+const actividad = 'Estoy aprendiendo JavaScript';
+console.log(actividad.split(' '));
+
+const hobbies = 'Leer, caminar, escuchar música, escribir, aprender a programar';
+console.log(hobbies.split(', '));
+
+const tweet = 'Aprendiendo JavaScript #JavaScript';
+console.log(tweet.split('#'));
