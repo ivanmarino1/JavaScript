af99e5d35818feee5a523f1932409487dde9c103
ivanmarino1 <ivanamarillamarino@gmail.com>
2024-01-14 22:44:06 -0300

    commit

---
+++ JavaScript/04-Numeros/js/03-app.js
@@ -0,0 +1,33 @@
+let resultado;
+
+// PI
+resultado = Math.PI;
+
+// Redondear
+resultado = Math.round(2.8); // Hacia arriba
+resultado = Math.round(2.2); // Hacia abajo
+
+// Redondear hacia arriba
+resultado = Math.ceil(2.1); 
+
+// Redondear hacia abajo
+resultado = Math.floor(2.9);
+
+// Raiz Cuadrada
+resultado = Math.sqrt(144);
+
+// Absoluto
+resultado = Math.abs(-500);
+
+// Potencia
+resultado = Math.pow(8, 3);
+
+// Minimo
+resultado = Math.min(3, 5);
+
+
+// Aleatorio
+resultado = Math.random();
+
+// Aleatorio dentro de un rango
+resultado = Math.floor(Math.random()* 30);
