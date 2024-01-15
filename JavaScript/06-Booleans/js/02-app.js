e081720858ce12b04fd474b5bc9cb0b1a16cdc5c
ivanmarino1 <ivanamarillamarino@gmail.com>
2024-01-15 20:48:48 -0300

    commit

---
+++ JavaScript/06-Booleans/js/02-app.js
@@ -0,0 +1,8 @@
+const boolean1 = true;
+const boolean2 = false;
+
+console.log(boolean1 === boolean2); // False
+
+console.log(boolean1 === true); // True
+console.log(boolean1 === 'true'); // False
+
